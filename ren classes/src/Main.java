5486d9e4ecb557c8e53f75da69c17ba301271453
renlimass <168127702+renlimass@users.noreply.github.com>
2024-04-25 15:37:20 -0300

    Add files via upload

---
+++ ren classes/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private String modelo;
+    private int capacidadePassageiros;
+    private int passageirosAtuais;
+
+
+    public Main(String modelo, int capacidadePassageiros) {
+        this.modelo = modelo;
+        this.capacidadePassageiros = capacidadePassageiros;
+        this.passageirosAtuais = 0;
+    }
+
+    public void embarcarPassageiros(int quantidade) {
+        if (passageirosAtuais + quantidade <= capacidadePassageiros) {
+            passageirosAtuais += quantidade;
+            System.out.println(quantidade + " passageiros embarcados com sucesso.");
+        } else {
+            System.out.println("Capacidade máxima de passageiros excedida.");
+        }
+    }
+
+
+    public void desembarcarPassageiros(int quantidade) {
+        if (quantidade <= passageirosAtuais) {
+            passageirosAtuais -= quantidade;
+            System.out.println(quantidade + " passageiros desembarcados com sucesso.");
+        } else {
+            System.out.println("Não há passageiros suficientes para desembarcar.");
+        }
+    }
+
+
+    public void informacoesAviao() {
+        System.out.println("Modelo do avião: " + modelo);
+        System.out.println("Capacidade de passageiros: " + capacidadePassageiros);
+        System.out.println("Passageiros atuais: " + passageirosAtuais);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Main aviao = new Main("Boeing 747", 400);
+
+
+        int opcao;
+        do {
+            System.out.println("\nEscolha uma opção:");
+            System.out.println("1. Embarcar passageiros");
+            System.out.println("2. Desembarcar passageiros");
+            System.out.println("3. Ver informações do avião");
+            System.out.println("0. Sair");
+
+            opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Quantos passageiros deseja embarcar?");
+                    int embarque = scanner.nextInt();
+                    aviao.embarcarPassageiros(embarque);
+                    break;
+                case 2:
+                    System.out.println("Quantos passageiros deseja desembarcar?");
+                    int desembarque = scanner.nextInt();
+                    aviao.desembarcarPassageiros(desembarque);
+                    break;
+                case 3:
+                    aviao.informacoesAviao();
+                    break;
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        } while (opcao != 0);
+
+        scanner.close();
+    }
+}
